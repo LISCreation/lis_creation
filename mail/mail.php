186cf1270221c4fe0dbdf5fc5ddaf5ff161714b8
LIS Creation <lis.lightinsky@gmail.com>
2018-10-26 21:50:16 +0300

    Add new mail modification [SMTP] sender

--- mail/mail.php
+++ mail/mail.php
@@ -1,39 +1,69 @@
 <?php
-//Post data
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\Exception;
+
+require 'PHPMailer/Exception.php';
+require 'PHPMailer/PHPMailer.php';
+require 'PHPMailer/SMTP.php';
+
+$mail = new PHPMailer(true);
+$mail->CharSet = 'utf-8';
 $name = $_POST['name'];
 $phone = $_POST['phone'];
 $email = $_POST['email'];
 
-//Mail info
-$dest = htmlspecialchars('lis.lightinsky@gmail.com');
-$subject = 'Заявка с сайта '.$_SERVER['SERVER_NAME'];
-$msg = htmlspecialchars("Пришла заявка с сайта $_SERVER[SERVER_NAME]:\n
-Имя: $name\n
-Номер телефона: $phone\n
-Электронная почта: $email\n");
-
-
-$response = 0;
-if(isset($name) && isset($phone) && isset($email)) {
-    if($name == '' || $phone == '' || $email == '') {
-        $response = 'empty';
-    } else {
-        if(strlen($name) < 3 || strlen($phone) < 9 || !filter_var($email, FILTER_VALIDATE_EMAIL)) {
-            $response = 'incorrect';
-        } else {
-            $response = 'send';
-            mail($dest, $subject, $msg, ['From' => "master@$_SERVER[SERVER_NAME]"]);
-        }
-    }
-}
-switch($response) {
-    case 'empty':
-        echo "<p class='bad'>Хорошая попытка! Но для начала заполните поля.</p>";
-        break;
-    case 'incorrect':
-        echo "<p class='bad'>Некоторые поля заполнены не правильно, проверьте правильность!</p>";
-        break;
-    case 'send':
-        echo "<p class='good'>Спасибо за заявку! Ждите звонка!</p>";
-}
-?>
+$mail->isSMTP();
+$mail->SMTPAuth = true;
+$mail->Host = 'smtp.mail.ru';
+$mail->Port = 465;
+$mail->SMTPSecure = 'ssl';
+$mail->Username = 'lis-creation@mail.ru';
+$mail->Password = '05x22003l';
+$mail->SetFrom('lis-creation@mail.ru', 'Бот рассылки LIS');
+$mail->AddAddress('lis.lightinsky@gmail.com', 'Gromov Alex');
+$mail->Subject = 'Поступила заявка с сайта lis-creation.pro';
+$mail->MsgHTML("
+<html>
+<head>
+	<meta http-equiv='Content-Type' content='text/html; charset=utf-8'>
+	<style type='text/css'>
+		table {
+			color: #fff;
+		}
+		table, th, td {
+			padding: 5px;
+			border-radius: 5px;
+		}
+		h3 {
+			background: #5313b8;
+		}
+		h3 > a {
+			color: #fff;
+		}
+	</style>
+</head>
+<body>
+	Заявка с сайта <a href='http://lis-creation.pro'>lis-creation</a>
+	Откройте описание
+	<table border='1' bgcolor='#5313b8'>
+		<tr>
+			<th>Тип заявки</th>
+			<th>Имя</th>
+			<th>Телефон</th>
+			<th>Email</th>
+		</tr>
+		<tr>
+			<td>Обратный звонок</td>
+			<td>$name</td>
+			<td>$phone</td>
+			<td>$email</td>
+		</tr>
+	</table>
+</body>
+</html>
+");
+if($mail->send()) :
+	echo "Сообщение отправлено!";
+else :
+	echo "Не отправлено";
+endif; 
