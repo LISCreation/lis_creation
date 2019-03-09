e821c96f075e9c18d95fd6f814d0b202aa99f551
LIS Creation <lis.lightinsky@gmail.com>
2019-03-09 23:46:14 +0300

    Update mail with composer

--- mail/mail.php
+++ mail/mail.php
@@ -5,9 +5,7 @@ use PHPMailer\PHPMailer\PHPMailer;
 use PHPMailer\PHPMailer\Exception;
 
 //Require
-require 'PHPMailer/Exception.php';
-require 'PHPMailer/PHPMailer.php';
-require 'PHPMailer/SMTP.php';
+require 'vendor/autoload.php';
 
 //Mail data
 $name = $_POST['name'];
